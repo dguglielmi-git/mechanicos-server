54fdd2ad00b14fb0e191028fff55f0d6b1433b2d
Daniel Guglielmi <54913286+dguglielmi-git@users.noreply.github.com>
2021-07-01 11:39:41 -0300

    Update resolver.js

--- src/gql/resolver.js
+++ src/gql/resolver.js
@@ -5,20 +5,20 @@ const tmpBudget = require('../controllers/tmpbudget');
 
 const resolvers = {
 	Query: {
-		getUser: 			(_, { id, username }) 	=> userController.getUser(id, username),
-		getTmpbudget: 		(_,) 					=> tmpBudget.getTmpbudget(),
-		getBudget: 			(_,) 					=> budget.getBudget(),
-		getBudget: 			(_,) 					=> budget.getBudgets(),
-		getBudgetnums: 		(_,) 					=> budget.getBudgetnums(),
+		getUser: 		(_, { id, username }) 	=> userController.getUser(id, username),
+		getTmpbudget: 		(_,) 			=> tmpBudget.getTmpbudget(),
+		getBudget: 		(_,) 			=> budget.getBudget(),
+		getBudget: 		(_,) 			=> budget.getBudgets(),
+		getBudgetnums: 		(_,) 			=> budget.getBudgetnums(),
 	},
 	Mutation: {
-		login: 				(_, { input }) 			=> userController.login(input),
-		register: 			(_, { input }) 			=> userController.register(input),
-		insertTmpBudget: 	(_, { input }) 			=> tmpBudget.insertTmpBudget(input),
-		insertBudget: 		(_, { input }) 			=> budget.insertBudget(input),
-		updateTmpBudget: 	(_, { input }) 			=> tmpBudget.updateTmpBudget(input),
-		emptyTmpBudget: 	(_,) 					=> tmpBudget.emptyTmpBudget(),
-		incBudgetNum: 		(_,) 					=> budget.incBudgetNum(),
+		login: 			(_, { input }) 		=> userController.login(input),
+		register: 		(_, { input }) 		=> userController.register(input),
+		insertTmpBudget: 	(_, { input }) 		=> tmpBudget.insertTmpBudget(input),
+		insertBudget: 		(_, { input }) 		=> budget.insertBudget(input),
+		updateTmpBudget: 	(_, { input }) 		=> tmpBudget.updateTmpBudget(input),
+		emptyTmpBudget: 	(_,) 			=> tmpBudget.emptyTmpBudget(),
+		incBudgetNum: 		(_,) 			=> budget.incBudgetNum(),
 	},
 };
 
